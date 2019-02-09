7b4292fe2e4146b9b80eb77378c61f025b8c7265
Wassim Khelifi <wassimkhelifi5@gmail.com>
2019-02-08 22:09:40 -0800

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 // Gulpfile
 /*	[CONVENTIONS]:
  *	--------------------------------------------------------------
- * 	Named are to be specific! They must indicate what they do (as verbs). 
+ * 	Named are to be specific! They must indicate what they do (as verbs).
  * 		- Use camelCase where the first word is in full lowercase (justLikeThis(...)).
  * 	Functions for Gulp to run ae written as:
  * 		gulp.task( 'taskName', function() {
  			//your code here. . .
- 		}); 
+ 		});
  *    	- It's a statement of: gulp.task('name', function);
- * 	Inside the main [.yml] file in the Repository, type inside the use area (at the bottom): 
+ * 	Inside the main [.yml] file in the Repository, type inside the use area (at the bottom):
  * 		- script: gulp takName
- * 
+ *
  * 	Make sure you have gulp-util installed on your command prompt/terminal (all of them)
  * 		- 1. Powershell
  * 		- 2. Command Prompt
  * 		- etc. . . .
- * 
+ *
  * For future testers or testers who have forgotten, you can install gulp via:
  * 		- 1. Going to your terminal (all/any)
  * 		- 2. Go to a suitable directory for you (easily accessed) and copy paste the code from this into there.
@@ -24,13 +24,20 @@
  * 			- 3.a npm install gulp
  * 			- 3.b (for some users): npm install gulp -g
  * 			- 3.c npm install gulp-util --save-dev
- * 
+ *
  * 	(: Happy Testing!
- * 	
- * 	WRITTEN BY: 
+ *
+ * 	WRITTEN BY:
  * 		Jin (PM)
  */
 //------------------------------------------------------------------
 
-//Code starts here: 
+//Code starts here:
 
+var gulp = require('gulp');
+
+//tasks
+
+gulp.task('default', function() {
+  console.log('GULP IS RUNNING')
+});
